28278d4d907d4812452604ad4bc9f7cf9830ed5a
Andrew <andy.birks@gmail.com>
2016-08-27 18:33:38 +0100

    To make it easier to develop our configuration, we can integrate a tool known as webpack-validator to our project.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+
 
 
 const PATHS = {
